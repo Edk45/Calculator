a2bac768945f86e4a3cd6b268863e1bff1b581da
Edk45 <kendalleric12@gmail.comm>
2023-03-18 14:50:13 -0400

    Webstuff added

--- mysiteDjango/polls/admin.py
+++ mysiteDjango/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub-date', 'question-text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
